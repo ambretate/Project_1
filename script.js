ae4b1cc76681e702161c982df585534da40c45af
Ambre Tate <ambretate@gmail.com>
2024-02-07 13:36:33 -0500

    first draft of index.html and API Key added to script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const url = "https://api.sunrise-sunset.org/json"
-const lat = 36.0
-const long = 4.4
+const sunURL = "https://api.sunrise-sunset.org/json";
+const latLongURL = "https://api.api-ninjas.com/v1/geocoding?city=";
+const city = "Brooklyn";
+const lat = 36.0;
+const long = 4.4;
+const apiKey = 'ifSDKbn6+C408rU06ll5GA==tI2dBZ1n24SpzCOZ';
 
 
-fetch(`${url}?lat=${lat}&lng=${long}`)
+fetch(`${sunURL}?lat=${lat}&lng=${long}`)
+    .then((res) => {
+        return res.json();
+    })
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log("Uh-oh! There has been an error!", err);
+    })
+
+fetch(`${latLongURL}${city}`, {headers: {
+    'X-Api-Key': `${apiKey}`,
+}})
     .then((res) => {
         return res.json();
     })
