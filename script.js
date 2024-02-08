00b2a0c9c0a98ab3d8cb3b3bc83e9806d4dda6a6
Ambre Tate <ambretate@gmail.com>
2024-02-08 12:09:57 -0500

    update script.js with chained fetch and adding list times through fetch response

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
+/*Chaining the Geocoding API fetch to pass latitude and longitude into the Sunrise-Sunset API fetch */
+
 const sunURL = "https://api.sunrise-sunset.org/json";
 const latLongURL = "https://api.api-ninjas.com/v1/geocoding?city=";
-const city = "Brooklyn";
-const lat = 36.0;
-const long = 4.4;
 const apiKey = 'ifSDKbn6+C408rU06ll5GA==tI2dBZ1n24SpzCOZ';
-
-
-fetch(`${sunURL}?lat=${lat}&lng=${long}`)
-    .then((res) => {
-        return res.json();
-    })
-    .then((res) => {
-        console.log(res);
-    })
-    .catch((err) => {
-        console.log("Uh-oh! There has been an error!", err);
-    })
+const city = "Brooklyn";
 
 fetch(`${latLongURL}${city}`, {headers: {
     'X-Api-Key': `${apiKey}`,
@@ -24,15 +12,61 @@ fetch(`${latLongURL}${city}`, {headers: {
         return res.json();
     })
     .then((res) => {
-        console.log(res);
-    })
+        const displayCity = document.querySelector(".displayCity");
+        displayCity.innerHTML = `${res[0].name}, ${res[0].state}`;
+
+        const lat = res[0].latitude;
+        const long = res[0].longitude;
+        fetch(`${sunURL}?lat=${lat}&lng=${long}`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                console.log(response);
+
+                const astroTwilightB = document.querySelector("#astroTwilightB");
+                astroTwilightB.innerHTML = response.results.astronomical_twilight_begin;
+
+                const nautiTwilightB = document.querySelector("#nautiTwilightB");
+                nautiTwilightB.innerHTML = response.results.nautical_twilight_begin;
+
+                const civilTwilightB = document.querySelector("#civilTwilightB");
+                civilTwilightB.innerHTML = response.results.civil_twilight_begin;
+
+                const sunrise = document.querySelector("#sunrise");
+                sunrise.innerHTML = response.results.sunrise;
+
+                const solarNoon = document.querySelector("#solarNoon");
+                solarNoon.innerHTML = response.results.solar_noon;
+
+                const sunset = document.querySelector("#sunset");
+                sunset.innerHTML = response.results.sunset;
+
+                const civilTwilightE = document.querySelector("#civilTwilightE");
+                civilTwilightE.innerHTML = response.results.civil_twilight_end;
+
+                const nautiTwilightE = document.querySelector("#nautiTwilightE");
+                nautiTwilightE.innerHTML = response.results.nautical_twilight_end;
+
+                const astroTwilightE = document.querySelector("#astroTwilightE");
+                astroTwilightE.innerHTML = response.results.astronomical_twilight_end;
+            })
+           
+            .catch((err) => {
+                console.log("Uh-oh! There has been an error!", err);
+            })
+        })
     .catch((err) => {
         console.log("Uh-oh! There has been an error!", err);
     })
+   
+
+//function displayList(times) {
+  //  console.log(times);
+
+
 
 //Form event handler for passing city into lat/long fetch, setting display city
-
-//Chain lat/long fetch into sun fetch
 
 //Function to set hour, minutes, amPm
 
